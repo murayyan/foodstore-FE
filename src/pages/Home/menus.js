e0709efcb3ef309b9e2baa68967746bbec6ef824
Muhammad Rayyan <muhammad.abhad@btpn.com>
2020-12-04 22:32:27 +0700

    add sidebar menu

---
+++ src/pages/Home/menus.js
@@ -0,0 +1,9 @@
+const menus = [
+  { icon: "/images/menus/semua.png", label: "Semua", id: "" },
+  { icon: "/images/menus/utama.png", label: "Utama", id: "utama" },
+  { icon: "/images/menus/minuman.png", label: "Minuman", id: "minuman" },
+  { icon: "/images/menus/snack.png", label: "Snack", id: "snack" },
+  { icon: "/images/menus/pastry.png", label: "Pastry", id: "pastry" },
+];
+// export supaya bisa dipakai di file lain
+export default menus;
