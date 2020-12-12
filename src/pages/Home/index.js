5e14d745633ff8e6b3c74fad00059c1dc597e2d7
Muhammad Rayyan <muhammad.abhad@btpn.com>
2020-12-12 12:36:23 +0700

    add product component and pagination

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,31 @@
 import * as React from "react";
-import { SideNav, LayoutSidebar } from "upkit";
+import {
+  SideNav,
+  LayoutSidebar,
+  Responsive,
+  CardProduct,
+  Pagination,
+} from "upkit";
+import { config } from "../../config";
 import menus from "./menus";
 import TopBar from "../../components/TopBar";
+import {
+  fetchProducts,
+  setPage,
+  goToNextPage,
+  goToPrevPage,
+} from "../../features/Products/actions";
+
+import { useDispatch, useSelector } from "react-redux";
 
 const Home = () => {
+  let dispatch = useDispatch();
+  let products = useSelector((state) => state.products);
+
+  React.useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch, products.currentPage]);
+
   return (
     <div>
       <LayoutSidebar
@@ -12,6 +34,30 @@ const Home = () => {
           <div className="w-full h-full mr-5 md:flex md:flex-row-reverse min-h- screen">
             <div className="w-full pb-10 pl-5 md:w-3/4">
               <TopBar />
+              <Responsive desktop={3} items="stretch">
+                {products.data.map((product, index) => {
+                  return (
+                    <div key={index} className="p-2">
+                      <CardProduct
+                        title={product.name}
+                        imgUrl={`${config.api_host}/upload/${product.image_url}`}
+                        price={product.price}
+                        // onAddToCart={(_) => null}
+                      />
+                    </div>
+                  );
+                })}
+              </Responsive>
+              <div className="my-10 text-center">
+                <Pagination
+                  totalItems={products.totalItems}
+                  page={products.currentPage}
+                  perPage={products.perPage}
+                  onChange={(page) => dispatch(setPage(page))}
+                  onNext={() => dispatch(goToNextPage())}
+                  onPrev={() => dispatch(goToPrevPage())}
+                />
+              </div>
             </div>
             <div className="w-full h-full bg-gray-100 border-r border-white shadow-lg md:w-1/4">
               Keranjang belanja di sini
