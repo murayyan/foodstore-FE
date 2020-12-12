22e05c823367819f4d8a46052df846a8140ef1b5
Muhammad Rayyan <muhammad.abhad@btpn.com>
2020-12-12 12:36:07 +0700

    add redux state product

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,14 @@
 import { combineReducers, createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import authReducer from "../features/Auth/reducer";
+import productReducer from "../features/Products/reducer";
 
 // buat composer enhancer untuk menghubungkan dengan Chrome DevTools Redux
 const composerEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducers = combineReducers({
   auth: authReducer,
+  products: productReducer,
 });
 
 // buat store, dan gunakan composerEnhancer + middleware thunk
